0ad9c40fdef4a552b04ea6b44a2238458f1fb5dc
Sultan Ahmad Khan <saksaikot@gmail.com>
2021-09-16 13:58:05 +0600

    initial project

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Initial Project
+
+### Please, please give me some time this project will be complete within 10.1.21
+
+# Assignment
+
+## 4.3 Integrate Social Login in the E- commerce Project
+
+| Criteria     | Specifications                       |
+| ------------ | ------------------------------------ |
+| Google OAuth | Include “Login with Google Button”   |
+| Facebook     | Include “Login with Facebook” Button |
---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv/config");
+const app = require("./app");
+const mongoose = require("mongoose");
+
+global.__basedir = __dirname;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("MongoDB Connection Failed!"));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}!`);
+});
